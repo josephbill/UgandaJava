ab71e21429b509d6a8f13ff2de1499428f806802
hutfarmkenya <hutfarmkenya@gmail.com>
2020-11-09 12:00:56 +0300

    My first commit for the android cohort JAVA

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main(String args[]){
+        //to print output in java
+        System.out.println("Hello Uganda welcome to the android accelerator program");
+        System.out.println("My name is Joseph");
+        System.out.println("I am your trainer " + "pleased to be here!");
+    }
+}
